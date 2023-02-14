2ffa800e0deb22f019b5f5ee6ddb655a0be040b1
LeonardoQ357 <Leonardo.quederoli.leme@hotmail.com>
2023-02-14 11:53:02 -0300

    Atualização

---
+++ aula1/Exemplo3.php
@@ -0,0 +1,15 @@
+<?php
+    $a = 1;
+
+    echo ++$a."<br>"; // pré incremento - Executa o incremento e mostra a$=2
+    echo $a++."<br>"; // pós incremento - Mostra e depois incrementa a$=3
+    echo $a."<br>"; // $a=3
+
+    echo "<br>";
+
+    $b = "ola";
+    echo ++$b."<br>"; // pré incremento
+    echo ++$b."<br>"; // pré incremento
+    echo ++$b."<br>"; // pré incremento
+
+?>
