848413550448248ffaf012c6a26be51ba3d49410
LeonardoQ357 <Leonardo.quederoli.leme@hotmail.com>
2023-02-14 11:01:26 -0300

    Meu primeiro programa PHP

---
+++ aula1/ola2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Aqui é codigo HTML !-->
+    <h1>Exemplo Olá Mundo</h1>
+    <?php 
+        // Aqui é codigo PHP
+        echo "<h2>Olá IFSP - Leonardo Quederoli Leme </h2>";
+    ?>
+</body>
+</html>
