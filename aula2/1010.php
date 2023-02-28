69c9c35d605ba86fd130ba668db91cf195e926de
LeonardoQ357 <leonardo.quederoli.leme@hotmail.com>
2023-02-28 12:18:43 -0300

    asd

--- aula2/1010.php
+++ aula2/1010.php
@@ -1,11 +1,16 @@
 <?php
-    $cod1 = readline();
-    $num1 = readline();
-    $val1 = readline();
-    $cod2 = readline();
-    $num2 = readline();
-    $val2 = readline();
-    $valf = $num1 * $val1 + $num2 * $val2;
+    $linha1 = readline();
+    $linha2 = readline();
+
+    $val1 = explode('',$linha1);
+    $qtd1 = intval($val1[1]);
+    $vlr1 = floatval($val1[2]);
+
+    $val2 = explode('',$linha2);
+    $qtd2 = intval($val2[1]);
+    $vlr2 = floatval($val2[2]);
+
+    $valf = $qtd1 * $vlr1 + $qtd2 * $vlr2;
     $valf = number_format($valf, 2, ".", "");
 
     echo "VALOR A PAGAR: R$ $valf\n";
--- aula2/1017.php
+++ aula2/1017.php
@@ -1,3 +1,8 @@
 <?php
-
+    $tg = readline();
+    $v = readline();
+    $cg = ($tg * $v) / 12;
+    $cg = number_format($cg, 3, ".", "");
+    
+    echo "$cg\n";
 ?>
