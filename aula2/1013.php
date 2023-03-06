5a98a0f374a41de3d9d33667879d5ab8fb0941af
LeonardoQ357 <leonardo.quederoli.leme@hotmail.com>
2023-03-06 09:09:20 -0300

    alt

--- aula2/1013.php
+++ aula2/1013.php
@@ -2,9 +2,9 @@
     $l1 = readline();
 
     $val1 = explode(' ', $l1);
-    $a = intval ($val1[0]);
-    $b = intval ($val1[1]);
-    $c = intval ($val1[2]);
+    $a = intval($val1[0]);
+    $b = intval($val1[1]);
+    $c = intval($val1[2]);
 
     $mab = ($a+$b+$a*$b($a-$b))/2;
     $mabc = ($mab+$c+$mab*$c($mab-$c))/2;
