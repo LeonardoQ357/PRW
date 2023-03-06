5faec1c3e2877f4cd7f9c436699de5f84c9e08fb
LeonardoQ357 <leonardo.quederoli.leme@hotmail.com>
2023-03-06 09:03:23 -0300

    alt

--- aula2/1012.php
+++ aula2/1012.php
@@ -1,7 +1,26 @@
 <?php
-    $r = readline();
-    $v = (4/3) * 3.14159 * ($r*$r*$r);
-    $v = number_format($v, 3, ".", "");
+    $l1 = readline();
 
-    echo "VOLUME = $v\n";
+    $val1 = explode(' ',$l1);
+    $a = floatval($val1[0]);
+    $b = floatval($val1[1]);
+    $c = floatval($val1[2]);
+
+
+    $tri = ($a*$c)/2;
+    $tri = number_format($tri, 3, ".", "");
+
+    $cir = 3.14159*($c*$c);
+    $cir = number_format($cir, 3, ".", "");
+
+    $tra = ($a + $b)*$c/2;
+    $tra = number_format($tra, 3, ".", "");
+
+    $qua = $b*$b;
+    $qua = number_format($qua, 3, ".", "");
+
+    $ret = $a * $b;
+    $ret = number_format($ret, 3, ".", "");
+
+    echo "TRIANGULO: $tri\nCIRCULO: $cir\nTRAPEZIO: $tra\nQUADRADO: $qua\nRETANGULO: $ret\n";
 ?>
