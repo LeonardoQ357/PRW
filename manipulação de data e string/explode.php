0420a05e358cf80b5512c48416c9ac40612307a6
LeonardoQ357 <leonardo.quederoli.leme@hotmail.com>
2023-03-21 10:46:03 -0300

    a

--- manipulação de data e string/explode.php
+++ manipulação de data e string/explode.php
@@ -6,4 +6,12 @@
     echo "Dia: ".$d[0]."<br>";
     echo "Mes: ".$d[1]."<br>";
     echo "Ano: ".$d[2]."<br>";
+
+    //Utilizar explode para separar as palavra utilizando <ESPAÇO>
+    $str = "IFSP Campus Birigui";
+    $i = explode(' ',$str);
+    
+    echo $i[0]."<br>";
+    echo $i[1]."<br>";
+    echo $i[2]."<br>";
 ?>
