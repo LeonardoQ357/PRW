fcbab18b461efb4a8692cffd311cf33c09da5f3a
LeonardoQ357 <leonardo.quederoli.leme@hotmail.com>
2023-03-28 10:28:35 -0300

    aaaaaaaaaaa

--- vetores e matrizes/percorrer_elementos_de_vetor.php
+++ vetores e matrizes/percorrer_elementos_de_vetor.php
@@ -1,10 +1,14 @@
 <?php
     // Exemplo para percorrer elementos de um vetor
     $alunos = array("Murilo", "Cássio", "Ricardo");
-
+    print_r($alunos); // Imprime todos elementos do vetor
+    echo "<br>";
     echo "Quantidade de elementos : ". count($alunos). "<br>"; 
+    echo "<hr>";
 
     for($x=0; $x < count($alunos); $x++){
-        echo $alunos[$x]."<br>";
+        echo "Aluno na posição $x: " .$alunos[$x]. "<br>";
+        echo "quantidade de caracters: ".strlen($alunos[$x])."<br>";
+        echo "<hr>";
     }
 ?>
