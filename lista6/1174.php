b20b3a1889194ddc0265ec32e87281fad8da2704
LeonardoQ357 <leonardo.quederoli.leme@hotmail.com>
2023-03-28 11:28:53 -0300

    atualizado

---
+++ lista6/1174.php
@@ -0,0 +1,11 @@
+<?php
+    for($i=0;$i <= 99; $i++){
+        $v[$i] = readline();
+    }
+
+    for($i=0;$i <= 99; $i++){
+        if($v[$i] <= 10){
+            echo "A[$i] = ".number_format($v[$i], 1, ".", "")."\n";
+        }else
+    }
+?>
