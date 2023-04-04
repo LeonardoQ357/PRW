e8d5da03319187c5a25f8a40ada9f9fdb6df5b2c
leonardoQ357 <leonardo.quederoli.leme@hotmail.com>
2023-04-04 09:33:03 -0300

    SORTE

--- lista3/1064.php
+++ lista3/1064.php
@@ -1,3 +1,15 @@
 <?php
+ for($i=1;$i<=6;$i++){
+    $x = readline();
 
+    if($x > 0){
+        $m += $x;
+        
+        $p++;
+    }
+ }
+
+ $m = $m/$p;
+
+ echo "$p valores positivos\n".number_format($m,1,".","")."\n";
 ?>
---
+++ lista3/1065.php
@@ -0,0 +1,11 @@
+<?php
+    for($i = 1; $i <= 5;$i++){
+        $x = readline();
+        
+        if($x%2 == 0){
+           $p++;
+        }
+    }
+
+    echo "$p valores pares\n";
+?>
