d1c1dc0cb670738f7f5b0191025b8e4b518a7dd2
leonardoQ357 <leonardo.quederoli.leme@hotmail.com>
2023-04-04 11:50:48 -0300

    lista3

--- lista3/1080.php
+++ lista3/1080.php
@@ -1,3 +1,19 @@
 <?php
+    $x = readline();
 
+    $m = $x;
+
+    $pm = 1;
+
+    for($i=2;$i <= 100;$i++){
+        $x = readline();
+
+        if($x > $m){
+            $m = $x;
+
+            $pm = $i;
+        }
+    }
+
+    echo "$m\n$pm\n";
 ?>
--- lista3/1094.php
+++ lista3/1094.php
@@ -1,3 +1,32 @@
 <?php
+    $n = readline();
 
+    $r = 0;
+    $s = 0;
+    $c = 0;
+
+    for($i=1;$i <= $n;$i++){
+        $v = explode(" ", readline());
+
+        if($v[1] == "R"){
+            $r += $v[0];
+        }else if($v[1] == "S"){
+            $s += $v[0];
+        }else if($v[1] == "C"){
+            $c += $v[0];
+        }
+    }
+
+    $t = $r + $s + $c;
+    $pr = ($r / $t) * 100;
+    $ps = ($s / $t) * 100;
+    $pc = ($c / $t) * 100;
+
+    echo "Total: $t cobaias\n";
+    echo "Total de coelhos: $c\n";
+    echo "Total de ratos: $r\n";
+    echo "Total de sapos: $s\n";
+    echo "Percentual de coelhos: ". number_format($pc, 2, ".", "") ." %\n";
+    echo "Percentual de ratos: ". number_format($pr, 2, ".", "") ." %\n";
+    echo "Percentual de sapos: ". number_format($ps, 2, ".", "") ." %\n";
 ?>
