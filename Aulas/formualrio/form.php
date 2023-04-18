020e0756461441bb06ae042e11e166047117943d
leonardoQ357 <leonardo.quederoli.leme@hotmail.com>
2023-04-18 09:34:13 -0300

    a

--- Aulas/formualrio/form.php
+++ Aulas/formualrio/form.php
@@ -7,6 +7,16 @@
     echo "<h1>$nome</h1>";
     echo "<h2>$idade</h2>";
     echo "<input value=$idade>";
+    
+    if(strlen($nome) < 20){
+        echo "<h3>Nome deve conter no minimo 20 caracteres</h3>";
+    }
+    
+    if($idade >= 18){
+        echo"<h3>Aluno maior de idade!</h3>";
+    }else{
+        echo"<h3>Aluno menor de idade!</h3>";
+    }
     //echo "Nome: " . $_GET['nome'] . "\nIdade: " . $_GET['idade'] . "\n";
 ?>
  <!-- Código HTML -->
