aa470a732c862aa1888115c3e9b35eb81811db2e
leonardoQ357 <leonardo.quederoli.leme@hotmail.com>
2023-04-25 11:38:19 -0300

    form

--- Listas/formularios/atividade2/form2.php
+++ Listas/formularios/atividade2/form2.php
@@ -1,3 +1,42 @@
 <?php
+    $text = $_POST['text'];
+    $textarea = $_POST['textarea'];
+    $radio = $_POST['radio'];
+    $select = $_POST['select'];
+
+        echo 
+    "<div>
+        <h3>Text</h3> 
+        $text
+    </div>
+    <div>
+        <h3>Textarea</h3>
+        $textarea
+    </div>
+    <div>
+        <h3>CheckBox</h3>";
+        
+            if(isset($_POST['checkbox1'])){
+                echo "CheckBox1: Marcada<br>";
+            }else{
+                echo "CheckBox1: Desmarcada<br>";
+            }
+
+            if(isset($_POST['checkbox2'])){
+                echo "CheckBox2: Marcada";
+            }else{
+                echo "CheckBox2: Desmarcada";
+            }
+
+        echo
+    "</div>
+    <div>
+        <h3>Radio</h3>
+        Opção escolhida: $radio
+    </div>
+    <div>
+        <h3>Select e Option</h3>
+        Opção escolhida: $select
+    </div>";
 
 ?>
